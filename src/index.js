6f3d76779a25b33067fcd677ac49669d4237eada
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-03-25 18:26:54 -0600

    Add change turn between players

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ class Square extends React.Component {
     }
     render() {
         return (
-            <button className="square" onClick={() => this.props.onClick()}>
+            <button className="square" onClick={this.props.onClick}>
                 {this.props.value}
             </button>
         );
@@ -23,12 +23,16 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
     renderSquare(i) {
         return (
@@ -40,7 +44,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
